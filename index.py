9cf07e58aa257df5e133e50a7006f6112e8c1980
sptj <sptj1993@gmail.com>
2018-02-21 16:31:00 +0800

    ok

--- index.py
+++ index.py
@@ -1,19 +1,13 @@
-import web
 import os, sys
-
 urls = (
     '/(.*)', 'hello'
 )
 
 
-class hello:
-    def GET(self, name):
-        if not name:
-            name = 'World'
-        return 'Hello, ' + name + '!'
 
 if __name__ == "__main__":
-    port = os.environ.get("PORT", "5000")
-    app = web.application(urls, globals())
-    web.httpserver.runsimple(app.wsgifunc(), ('0.0.0.0', int(port)))
-    app.run()
+    print("hello")
+    # port = os.environ.get("PORT", "5000")
+    # app = web.application(urls, globals())
+    # web.httpserver.runsimple(app.wsgifunc(), ('0.0.0.0', int(port)))
+    # app.run()
