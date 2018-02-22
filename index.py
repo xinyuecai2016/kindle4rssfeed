3ad49291227297213f4ad867b912e23d77acf6d4
sptj <sptj1993@gmail.com>
2018-02-22 10:43:43 +0800

    ok

--- index.py
+++ index.py
@@ -1,13 +1,35 @@
+import web
 import os, sys
+
 urls = (
     '/(.*)', 'hello'
 )
 
 
+class hello:
+    def GET(self, name):
+        if not name:
+            name = 'World'
+        return 'Hello, ' + name + '!'
 
 if __name__ == "__main__":
-    print("hello")
-    # port = os.environ.get("PORT", "5000")
-    # app = web.application(urls, globals())
-    # web.httpserver.runsimple(app.wsgifunc(), ('0.0.0.0', int(port)))
-    # app.run()
+    port = os.environ.get("PORT", "5000")
+    app = web.application(urls, globals())
+    web.httpserver.runsimple(app.wsgifunc(), ('0.0.0.0', int(port)))
+    app.run()
+# import os
+# import web
+# from weixinInterface import WeixinInterface
+#
+# urls = (
+# '/weixin','WeixinInterface'
+# )
+#
+# app_root = os.path.dirname(__file__)
+# templates_root = os.path.join(app_root, 'templates')
+# render = web.template.render(templates_root)
+# if __name__ == "__main__":
+#     port = os.environ.get("PORT", "5000")
+#     app = web.application(urls, globals())
+#     web.httpserver.runsimple(app.wsgifunc(), ('0.0.0.0', int(port)))
+#     app.run()
