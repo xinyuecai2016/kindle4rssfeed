fb55a7c8d25591018baf6236a78c76f3cc075768
sptj <sptj1993@gmail.com>
2018-02-22 16:00:50 +0800

    ok

--- index.py
+++ index.py
@@ -1,13 +1,24 @@
 import web
-import os, sys
+import os, sys, pytz, time ,datetime
+from kindle4rss_deliver import kindle4rss_deliver
 urls = (
     '/', 'index'
 )
+def is_the_right_time_to_deliver():
+    tz = pytz.timezone('Asia/Shanghai')
+    current_hour = datetime.datetime.now(tz).hour
+    if(current_hour==7):
+        return True
+    else:
+        return False
 class index:
     def GET(self):
-        print("A visit encounter\n")
-        return 'index '
-
+        if(is_the_right_time_to_deliver()):
+            kindle4rss_deliver()
+        return 'index'
+    def POST(self):
+        print(web.data())
+        return 'index'
 if __name__ == "__main__":
     port = os.environ.get("PORT", "5000")
     app = web.application(urls, globals())
